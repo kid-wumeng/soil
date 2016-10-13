f464e908ff5b99d56e3ce36eddd253f17869f15d
KID <kid.wumeng@gmail.com>
2016-10-14 00:43:41 +0800

    Add less

--- src/Playground.vue
+++ src/Playground.vue
@@ -1,9 +1,21 @@
 <template lang="pug">
   ListX
+    div.d1
+    div.d2
 </template>
 
 <script lang="coffee">
 </script>
 
-<style>
+<style lang="less">
+  .d1 {
+    width: 200px;
+    height: 200px;
+    background-color: red;
+  }
+  .d2 {
+    width: 300px;
+    height: 100px;
+    background-color: blue
+  }
 </style>
--- src/components/ListX/index.vue
+++ src/components/ListX/index.vue
@@ -1,12 +1,25 @@
 <template lang="pug">
-  div ListX
+  .soil-list-x
+    .wrap
+      .list
+        slot
 </template>
 
 <script lang="coffee">
 </script>
 
-<style>
-  div{
-    background-color: red;
+<style lang="less">
+  .soil-list-x {
+    outline: 1px dashed #000;
+    .wrap {
+      outline: 1px dashed #000;
+      .list {
+        outline: 1px dashed #000;
+        display: flex;
+        & > * {
+          outline: 1px dashed #000;
+        }
+      }
+    }
   }
 </style>
