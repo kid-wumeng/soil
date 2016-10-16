e56281412090e276d5eb9f43563660441315b143
KID <kid.wumeng@gmail.com>
2016-10-17 00:28:45 +0800

    Add ComponentDemo

---
+++ src/assets/ComponentDemo.vue
@@ -0,0 +1,23 @@
+<template lang="jade">
+
+  .soil-
+
+</template>
+
+
+
+<script lang="coffee">
+
+  module.exports =
+
+    mounted: ->
+
+</script>
+
+
+
+<style lang="less">
+
+  @import "../../assets/common";
+
+</style>
